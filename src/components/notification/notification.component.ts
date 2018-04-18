b74a85eaad13546137889f8e20075865a9ace983
BARHOUMI <hichemhlm@hotmail.com>
2018-04-18 14:48:07 +0200

    Progress Bar is infinite
    Change logic for progress Bar and update time

--- src/components/notification/notification.component.ts
+++ src/components/notification/notification.component.ts
@@ -9,10 +9,10 @@ import {
   TemplateRef,
   ViewEncapsulation
 } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
-import {Notification} from '../../interfaces/notification.type';
-import {NotificationsService} from '../../services/notifications.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Notification } from '../../interfaces/notification.type';
+import { NotificationsService } from '../../services/notifications.service';
 
 @Component({
   selector: 'simple-notification',
@@ -21,86 +21,86 @@ import {NotificationsService} from '../../services/notifications.service';
     trigger('enterLeave', [
 
       // Fade
-      state('fade', style({opacity: 1})),
+      state('fade', style({ opacity: 1 })),
       transition('* => fade', [
-        style({opacity: 0}),
+        style({ opacity: 0 }),
         animate('400ms ease-in-out')
       ]),
-      state('fadeOut', style({opacity: 0})),
+      state('fadeOut', style({ opacity: 0 })),
       transition('fade => fadeOut', [
-        style({opacity: 1}),
+        style({ opacity: 1 }),
         animate('300ms ease-in-out')
       ]),
 
       // Enter from top
-      state('fromTop', style({opacity: 1, transform: 'translateY(0)'})),
+      state('fromTop', style({ opacity: 1, transform: 'translateY(0)' })),
       transition('* => fromTop', [
-        style({opacity: 0, transform: 'translateY(-5%)'}),
+        style({ opacity: 0, transform: 'translateY(-5%)' }),
         animate('400ms ease-in-out')
       ]),
-      state('fromTopOut', style({opacity: 0, transform: 'translateY(5%)'})),
+      state('fromTopOut', style({ opacity: 0, transform: 'translateY(5%)' })),
       transition('fromTop => fromTopOut', [
-        style({opacity: 1, transform: 'translateY(0)'}),
+        style({ opacity: 1, transform: 'translateY(0)' }),
         animate('300ms ease-in-out')
       ]),
 
       // Enter from right
-      state('fromRight', style({opacity: 1, transform: 'translateX(0)'})),
+      state('fromRight', style({ opacity: 1, transform: 'translateX(0)' })),
       transition('* => fromRight', [
-        style({opacity: 0, transform: 'translateX(5%)'}),
+        style({ opacity: 0, transform: 'translateX(5%)' }),
         animate('400ms ease-in-out')
       ]),
-      state('fromRightOut', style({opacity: 0, transform: 'translateX(-5%)'})),
+      state('fromRightOut', style({ opacity: 0, transform: 'translateX(-5%)' })),
       transition('fromRight => fromRightOut', [
-        style({opacity: 1, transform: 'translateX(0)'}),
+        style({ opacity: 1, transform: 'translateX(0)' }),
         animate('300ms ease-in-out')
       ]),
 
       // Enter from bottom
-      state('fromBottom', style({opacity: 1, transform: 'translateY(0)'})),
+      state('fromBottom', style({ opacity: 1, transform: 'translateY(0)' })),
       transition('* => fromBottom', [
-        style({opacity: 0, transform: 'translateY(5%)'}),
+        style({ opacity: 0, transform: 'translateY(5%)' }),
         animate('400ms ease-in-out')
       ]),
-      state('fromBottomOut', style({opacity: 0, transform: 'translateY(-5%)'})),
+      state('fromBottomOut', style({ opacity: 0, transform: 'translateY(-5%)' })),
       transition('fromBottom => fromBottomOut', [
-        style({opacity: 1, transform: 'translateY(0)'}),
+        style({ opacity: 1, transform: 'translateY(0)' }),
         animate('300ms ease-in-out')
       ]),
 
       // Enter from left
-      state('fromLeft', style({opacity: 1, transform: 'translateX(0)'})),
+      state('fromLeft', style({ opacity: 1, transform: 'translateX(0)' })),
       transition('* => fromLeft', [
-        style({opacity: 0, transform: 'translateX(-5%)'}),
+        style({ opacity: 0, transform: 'translateX(-5%)' }),
         animate('400ms ease-in-out')
       ]),
-      state('fromLeftOut', style({opacity: 0, transform: 'translateX(5%)'})),
+      state('fromLeftOut', style({ opacity: 0, transform: 'translateX(5%)' })),
       transition('fromLeft => fromLeftOut', [
-        style({opacity: 1, transform: 'translateX(0)'}),
+        style({ opacity: 1, transform: 'translateX(0)' }),
         animate('300ms ease-in-out')
       ]),
 
       // Rotate
-      state('scale', style({opacity: 1, transform: 'scale(1)'})),
+      state('scale', style({ opacity: 1, transform: 'scale(1)' })),
       transition('* => scale', [
-        style({opacity: 0, transform: 'scale(0)'}),
+        style({ opacity: 0, transform: 'scale(0)' }),
         animate('400ms ease-in-out')
       ]),
-      state('scaleOut', style({opacity: 0, transform: 'scale(0)'})),
+      state('scaleOut', style({ opacity: 0, transform: 'scale(0)' })),
       transition('scale => scaleOut', [
-        style({opacity: 1, transform: 'scale(1)'}),
+        style({ opacity: 1, transform: 'scale(1)' }),
         animate('400ms ease-in-out')
       ]),
 
       // Scale
-      state('rotate', style({opacity: 1, transform: 'rotate(0deg)'})),
+      state('rotate', style({ opacity: 1, transform: 'rotate(0deg)' })),
       transition('* => rotate', [
-        style({opacity: 0, transform: 'rotate(5deg)'}),
+        style({ opacity: 0, transform: 'rotate(5deg)' }),
         animate('400ms ease-in-out')
       ]),
-      state('rotateOut', style({opacity: 0, transform: 'rotate(-5deg)'})),
+      state('rotateOut', style({ opacity: 0, transform: 'rotate(-5deg)' })),
       transition('rotate => rotateOut', [
-        style({opacity: 1, transform: 'rotate(0deg)'}),
+        style({ opacity: 1, transform: 'rotate(0deg)' }),
         animate('400ms ease-in-out')
       ])
     ])
@@ -138,12 +138,11 @@ export class NotificationComponent implements OnInit, OnDestroy {
 
   private stopTime = false;
   private timer: any;
-  private steps: number;
-  private speed: number;
-  private count = 0;
-  private start: any;
+  private framesPerSecond: number = 40;
+  private sleepTime: number;
+  private startTime: number;
+  private endTime: number;
 
-  private diff: any;
   private icon: string;
 
   constructor(
@@ -151,7 +150,7 @@ export class NotificationComponent implements OnInit, OnDestroy {
     private domSanitizer: DomSanitizer,
     private cdr: ChangeDetectorRef,
     private zone: NgZone
-  ) {}
+  ) { }
 
   ngOnInit() {
     if (this.item.override) {
@@ -178,10 +177,10 @@ export class NotificationComponent implements OnInit, OnDestroy {
   }
 
   startTimeOut(): void {
-    this.steps = this.timeOut / 10;
-    this.speed = this.timeOut / this.steps;
-    this.start = new Date().getTime();
-    this.zone.runOutsideAngular(() => this.timer = setTimeout(this.instance, this.speed));
+    this.sleepTime = 1000 / this.framesPerSecond /* ms */;
+    this.startTime = new Date().getTime();
+    this.endTime = this.startTime + this.timeOut;
+    this.zone.runOutsideAngular(() => this.timer = setTimeout(this.instance, this.sleepTime));
   }
 
   onEnter(): void {
@@ -193,7 +192,7 @@ export class NotificationComponent implements OnInit, OnDestroy {
   onLeave(): void {
     if (this.pauseOnHover) {
       this.stopTime = false;
-      this.zone.runOutsideAngular(() => setTimeout(this.instance, (this.speed - this.diff)));
+      this.zone.runOutsideAngular(() => setTimeout(this.instance, this.sleepTime));
     }
   }
 
@@ -223,17 +222,17 @@ export class NotificationComponent implements OnInit, OnDestroy {
   }
 
   private instance = () => {
-    this.diff = (new Date().getTime() - this.start) - (this.count * this.speed);
+    var now = new Date().getTime();
 
-    if (this.count++ === this.steps) {
+    if (this.endTime < now) {
       this.remove();
       this.item.timeoutEnd!.emit();
     } else if (!this.stopTime) {
       if (this.showProgressBar) {
-        this.progressWidth += 100 / this.steps;
+        this.progressWidth = Math.min((now - this.startTime + this.sleepTime /* We add this.sleepTime just to have 100% before close */) * 100 / this.timeOut, 100);
       }
 
-      this.timer = setTimeout(this.instance, (this.speed - this.diff));
+      this.timer = setTimeout(this.instance, this.sleepTime);
     }
     this.zone.run(() => this.cdr.detectChanges());
   }
@@ -251,11 +250,11 @@ export class NotificationComponent implements OnInit, OnDestroy {
 
   private contentType(item: any, key: string) {
     if (item instanceof TemplateRef) {
-      this[key] = item;
+      (this as any)[key] = item;
     } else {
-      this[key] = this.domSanitizer.bypassSecurityTrustHtml(item);
+      (this as any)[key] = this.domSanitizer.bypassSecurityTrustHtml(item);
     }
 
-    this[key + 'IsTemplate'] = item instanceof TemplateRef;
+    (this as any)[key + 'IsTemplate'] = item instanceof TemplateRef;
   }
 }
